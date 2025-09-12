6d575d3e48bcd1d71e3e5f3ec729829e8407398c
martinperassi43 <martinperassi4333@gmail.com>
2025-09-12 01:54:01 -0300

    modificacion

--- practica/borrador.java
+++ practica/borrador.java
@@ -1,6 +1,6 @@
-
-
-
-jkladjaskljdklasjdjñaslkdulfiaiofa
-pasojkldaskjlñda
-lkasdjdklasjkldjlasdkasjld
+public class Borrador{
+    public static void main(String [] args){
+        System.err.println("hola mundo");
+        System.err.println("chau mundo");
+    }
+}

255799b79d1093e5dbbcbcdd9c08349f0d2a1571
martinperassi43 <martinperassi4333@gmail.com>
2025-09-12 01:55:49 -0300

    Update borrador.java

--- practica/borrador.java
+++ practica/borrador.java
@@ -2,5 +2,7 @@ public class Borrador{
     public static void main(String [] args){
         System.err.println("hola mundo");
         System.err.println("chau mundo");
+        System.err.println("chau mundo");System.err.println("chau mundo");
+    
     }
 }

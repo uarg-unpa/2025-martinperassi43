af918b639a7d496fe7cf2026df160e6876fe68ac
martinperassi43 <martinperassi4333@gmail.com>
2025-09-17 22:56:35 +0000

    actualizo practica (clase hola )mostrando mensaje en pantalla

--- practica/borrador.java
+++
@@ -1,8 +0,0 @@
-public class Borrador{
-    public static void main(String [] args){
-        System.err.println("hola mundo");
-        System.err.println("chau mundo");
-        System.err.println("chau mundo");System.err.println("chau mundo");
-    
-    }
-}

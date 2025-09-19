26b3ca5007246f299210f17855214e64a52fa615
martinperassi43 <martinperassi4333@gmail.com>
2025-09-19 11:06:00 -0300

    practica de scanner con nombre, edad y sueldo

---
+++ practica/Practica_clases.java
@@ -0,0 +1,16 @@
+public class Practica_clases{
+    //declaracion de atributos
+
+    String calle;
+    String sector;
+    int numero;
+
+    /*MODIFICACORES DE ACCESO: Restringen el acceso a los atributos de una clase. 
+     * private:privado
+     * public:publico
+    */
+
+    
+
+}
+
